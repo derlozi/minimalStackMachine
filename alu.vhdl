51b7f2c592d44adc2833f97fccd4f5eb1e3911ea
derlozi <33962275+derlozi@users.noreply.github.com>
2024-02-26 16:19:46 +0100

    Rewrite ALU to fit with newer stack

--- alu.vhdl
+++ alu.vhdl
@@ -5,7 +5,7 @@ use IEEE.numeric_std.all;
 entity alu is
   port (
     i_clk : in std_logic;
-    o_eqzero, o_resReady, o_inc, o_dec, o_res : out std_logic;
+    o_eqzero, o_resReady, o_inc, o_dec1, o_dec2, o_res : out std_logic;
     i_tos, i_btos, i_dataIn : in std_logic_vector(15 downto 0); 
     i_inst : in std_logic_vector(3 downto 0);
     o_result : out std_logic_vector(15 downto 0)
@@ -19,7 +19,8 @@ begin
     process(i_clk)--result calculator process
     begin
         if(rising_edge(i_clk))then
-            o_dec <= '0';
+            o_dec1 <= '0';
+            o_dec2 <= '0';
             o_inc <= '0';
             o_res <= '0';
             o_resReady <= '0';
@@ -36,40 +37,47 @@ begin
                     o_resReady <= '1';
                 when "0011" =>--pop tos
                     r_res <= (others => '0');
-                    o_dec <= '1';
+                    o_dec1 <= '1';
                     o_resReady <= '1';
                 when "0100" =>--add
                     r_res <= std_logic_vector(signed(i_tos) + signed(i_btos));
                     o_resReady <= '1';
+                    o_dec2 <= '1';
                     o_inc <= '1';
                 when "0101" =>--invert
                     r_res <= NOT(i_tos);
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "0110" =>--2s complement
                     r_res <= std_logic_vector(signed(NOT(i_tos))+1);
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "0111" =>--multiply
                         --!TODO
                 when "1000" => --and
                     r_res <= i_tos and i_btos;
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "1001" => --or
                     r_res <= i_tos or i_btos;
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "1010" => --div
                     --!TODO
                 when "1011" => --right shift 
                     r_res <= std_logic_vector(shift_right(signed(i_tos),1));
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "1100" => --left shift
                     r_res <= std_logic_vector(shift_left(signed(i_tos),1));
                     o_resReady <= '1';
                     o_inc <= '1';
+                    o_dec1 <= '1';
                 when "1101" => --jnz
                 when "1110" => --push to RAM
                 when "1111" => --load from RAM
